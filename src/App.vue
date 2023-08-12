bfab2b55609369055f63f6cf4bb1b94cf567e732
scava <galsanov.bul888@gmail.com>
2023-08-13 02:14:23 +0700

    Chracters Gallery

--- src/App.vue
+++ src/App.vue
@@ -143,4 +143,19 @@ img {
   pointer-events: none;
 }
 
+.overlay {
+  position: fixed;
+  z-index: 2000;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(255, 255, 255, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overscroll-behavior: contain;
+
+}
+
 </style>
